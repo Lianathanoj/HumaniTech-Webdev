47813bee97c2eedba52dc31a39e2e1bf9ae1b3ee
Lianathanoj <jonathan.hahaha@gmail.com>
2016-04-02 20:11:14 -0400

    routes added

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET home page. */
+router.get('/main', function(req, res, next) {
+  res.render('main', { title: 'Express' });
+});
+
+
+/* GET home page. */
+router.get('/page3', function(req, res, next) {
+  res.render('page3', { title: 'Express' });
+});
+
+
 module.exports = router;
